96923b1eb545651d3a79a6d9bb8550486d815564
Asta P <astpetr@gmail.com>
2016-10-05 16:48:13 +0300

    from home

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,35 +1,39 @@
 class Board
 	def initialize
-		arr = []
-		players[player_a] = :playing
-		players[player_b] = :playing
+		signs = []
 	end 
+
+	def assign_sign
+
+	end
 
 	def write
 	end
 
-	def tikrinti
+	def win?
 	end
 end
 
 class Player
-	attr_accessor :name
+	attr_accessor :name, :sign, :id
 
-	def player_state
-	end
-
-	def won?
+	def state
 	end
 
 end
 
+game = Board.new
 player_a = Player.new
 player_b = Player.new
 puts "Player 1 name:"
 player_a.name = gets.chomp
+player_a.id = 1
 puts "PLayer 2 name"
 player_b.name = gets.chomp
-game = Board.new
-if !won?
-	puts ""
+player_b.id = 2
+# kuris eis pirmas
+pirmas = rand(1..2)
+puts pirmas
+
+
 
