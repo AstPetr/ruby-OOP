e5b287534ee544e433c3e9d547d2a0f9911e8bbd
AstPetr <astpetr@gmail.com>
2016-10-06 18:31:17 +0300

    game starting

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,23 +1,47 @@
 class Board
 	def initialize
-		signs = []
+		@signs = []
 	end 
 
-	def assign_sign
-
+	def random
+		first = rand(2)
 	end
 
-	def write
+	def start(player_a, player_b)
+		puts "#{player_a.name} goes first"
+		puts "#{player_a.name} choose sign"
+		player_a.sign = gets.chomp
+		puts "#{player_b.name} choose sign"
+		player_b.sign = gets.chomp
+		puts "Start game"
+		self.write(player_a)
+	end
+
+	def write(player)
+		while !win?
+			place = gets.chomp.split("")
+			@signs [place[0].to_i][place[1].to_i] = player.sign;
+		end
 	end
 
 	def win?
+		false
 	end
+
 end
 
 class Player
 	attr_accessor :name, :sign, :id
 
+	def initialize
+
+	end
+
 	def state
+	end
+
+	def first?(first)
+		self.id == first
 	end
 
 end
@@ -27,13 +51,24 @@ player_a = Player.new
 player_b = Player.new
 puts "Player 1 name:"
 player_a.name = gets.chomp
-player_a.id = 1
-puts "PLayer 2 name"
+player_a.id = 0
+puts "Player 2 name"
 player_b.name = gets.chomp
-player_b.id = 2
+player_b.id = 1
 # kuris eis pirmas
-pirmas = rand(1..2)
-puts pirmas
+first = game.random
+# puts first
+if player_a.first?(first)
+	game.start(player_a, player_b)
+elsif player_b.first?(first)   #nebutina bet del viso pikto
+	game.start(player_b, player_a)
+end
+
+
+
+
+
+
 
 
 
