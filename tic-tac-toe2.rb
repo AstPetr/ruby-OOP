f1d28458bc2695ce684e8bd63b10184abbda4a91
Asta P <astpetr@gmail.com>
2016-10-11 15:29:05 +0300

    simple AI

--- tic-tac-toe2.rb
+++ tic-tac-toe2.rb
@@ -8,7 +8,18 @@ class Game
   def initialize
     @signs = [ ["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"] ]
   end 
-
+  # Ask user if he wants to play with Ai or second player 
+  def secondplayer
+    puts "Do you want to play with AI? y/n?"
+    ats = gets.chomp
+    if ats == "y"
+      player = Computer.new
+    else
+      player = Player.new
+    end
+    player
+  end
+  # Creates player array with random positions
   def player_array(player_a, player_b)
     players = []
     players[rand(2)] = player_a
@@ -19,7 +30,7 @@ class Game
     end
     players
   end
-
+  # Starts game by anouncing who goes first and asigns signs
   def start(players)
     puts "#{players[0].name} goes first, sign - x"
     players[0].sign = "x "
@@ -27,7 +38,7 @@ class Game
     players[1].sign = "o "
     puts "Start game"
   end
-
+  # Plays game until someone wins
   def play(players)
     while !win?
       players.each do |player|
@@ -41,19 +52,31 @@ class Game
       end
     end
   end
-
+  # Writes chosen position to cell
   def write(player)
-    place = gets.chomp.split("")
-    @signs[place[0].to_i-1][place[1].to_i-1] = player.sign
+    begin
+      position = player.choose_position 
+    end until self.empty_cell?(position)
+    @signs[position[0].to_i-1][position[1].to_i-1] = player.sign
   end
-
+  # Checks if cell is empty 
+  def empty_cell?(position)
+    case @signs[position[0].to_i-1][position[1].to_i-1]
+    when "x " 
+      return false
+    when "o " 
+      return false
+    else return true
+    end
+  end
+  # Draws game board
   def draw
     @signs.each do |i|
       puts "#{i[0]}|#{i[1]}|#{i[2]}"
       puts "--|--|--"
     end
   end
-
+  # Checks if someone won
   def win?
     tikrinimai = [ [@signs[0][0], @signs[1][1], @signs[2][2]], [@signs[0][2], @signs[1][1], @signs[2][0]], @signs[0], @signs[1], @signs[2], @signs.collect{|i| i[0]}, @signs.collect{|i| i[1]}, @signs.collect{|i| i[2]}] 
     tikrinimai.each do |i|
@@ -74,11 +97,29 @@ class Player
     @name = gets.chomp
   end
   
+  def choose_position
+    gets.chomp.split("")
+  end
+
+end
+
+class Computer < Player
+  def initialize
+    puts "Hello, I'm friendly AI"
+    @name = "Friend"
+  end
+
+  def choose_position
+    (rand(1..3).to_s+rand(1..3).to_s).split("")
+  end
+
 end
 
 game = Game.new
+
 player_a = Player.new
-player_b = Player.new
+player_b = game.secondplayer
+
 players = game.player_array(player_a, player_b)
 game.start(players)
 game.play(players)
