83eceb89154752ba43df30be05a8ebf3c26c8359
Asta P <astpetr@gmail.com>
2016-10-11 14:16:09 +0300

    indentation

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -5,81 +5,74 @@ class Array
 end
 
 class Board
-	def initialize
-		@signs = [ ["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"] ]
-	end 
+  def initialize
+    @signs = [ ["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"] ]
+  end 
 
-	def start(player_a, player_b)
-		puts "#{player_a.name} goes first, sign - x"
-		player_a.sign = "x "
-		puts "#{player_b.name} goes second, sign - o"
-		player_b.sign = "o "
-		puts "Start game"
-		self.play(player_a, player_b)
-	end
+  def start(player_a, player_b)
+    puts "#{player_a.name} goes first, sign - x"
+    player_a.sign = "x "
+    puts "#{player_b.name} goes second, sign - o"
+    player_b.sign = "o "
+    puts "Start game"
+    self.play(player_a, player_b)
+  end
 
-	def play(player_a, player_b)
-		while !win?
-			if self.input_ok?(player_a)
-				self.draw
-				puts "#{player_a.name} turn"
-				self.write(player_a)
-			end
-			if self.input_ok?(player_b) && !win?
-				self.draw
-				puts "#{player_b.name} turn"
-				self.write(player_b)
-			end
-		end
-	end
+  def play(player_a, player_b)
+    while !win?
+      if self.input_ok?(player_a)
+        self.draw
+        puts "#{player_a.name} turn"
+        self.write(player_a)
+      end
+      if self.input_ok?(player_b) && !win?
+        self.draw
+        puts "#{player_b.name} turn"
+        self.write(player_b)
+      end
+    end
+  end
 
-	def random_player
-		first = rand(2)
-	end
+  def random_player
+    first = rand(2)
+  end
 
-	def input_ok?(player)
-		true
-	end
+  def input_ok?(player)
+    true
+  end
 
-	def write(player)
-		place = gets.chomp.split("")
-		@signs[place[0].to_i-1][place[1].to_i-1] = player.sign;
-		 #Cia bus lentele puts @signs
-	end
+  def write(player)
+    place = gets.chomp.split("")
+    @signs[place[0].to_i-1][place[1].to_i-1] = player.sign;
+     #Cia bus lentele puts @signs
+  end
 
-	def draw
-		@signs.each do |i|
-			puts "#{i[0]}|#{i[1]}|#{i[2]}"
-			puts "--|--|--"
-		end
-	end
+  def draw
+    @signs.each do |i|
+      puts "#{i[0]}|#{i[1]}|#{i[2]}"
+      puts "--|--|--"
+    end
+  end
 
-	def win?
-		tikrinimai = [ [@signs[0][0], @signs[1][1], @signs[2][2]], [@signs[0][2], @signs[1][1], @signs[2][0]], @signs[0], @signs[1], @signs[2], @signs.collect{|i| i[0]}, @signs.collect{|i| i[1]}, @signs.collect{|i| i[2]}] 
-		tikrinimai.each do |i|
-			if i.same_values?
-				self.draw
-				return true
-			end
-		end
-		false
-	end
+  def win?
+    tikrinimai = [ [@signs[0][0], @signs[1][1], @signs[2][2]], [@signs[0][2], @signs[1][1], @signs[2][0]], @signs[0], @signs[1], @signs[2], @signs.collect{|i| i[0]}, @signs.collect{|i| i[1]}, @signs.collect{|i| i[2]}] 
+    tikrinimai.each do |i|
+      if i.same_values?
+        self.draw
+        return true
+      end
+    end
+    false
+  end
 
 end
 
 class Player
-	attr_accessor :name, :sign, :id
+  attr_accessor :name, :sign, :id
 
-	def initialize
-
-	end
-
-	def state
-	end
-
-	def first?(first)
-		self.id == first
-	end
+  def first?(first)
+    self.id == first
+  end
 
 end
 
@@ -96,9 +89,9 @@ player_b.id = 1
 first = game.random_player
 # puts first
 if player_a.first?(first)
-	game.start(player_a, player_b)
+  game.start(player_a, player_b)
 elsif player_b.first?(first)   #nebutina bet del viso pikto
-	game.start(player_b, player_a)
+  game.start(player_b, player_a)
 end
 
 
--- tic-tac-toe2.rb
+++ tic-tac-toe2.rb
@@ -5,75 +5,75 @@ class Array
 end
 
 class Game
-	def initialize
-		@signs = [ ["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"] ]
-	end 
+  def initialize
+    @signs = [ ["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"] ]
+  end 
 
-	def player_array(player_a, player_b)
-		players = []
-		players[rand(2)] = player_a
-		if players[0].nil?
-			players[0] = player_b
-		else
-			players[1] = player_b
-		end
-		players
-	end
+  def player_array(player_a, player_b)
+    players = []
+    players[rand(2)] = player_a
+    if players[0].nil?
+      players[0] = player_b
+    else
+      players[1] = player_b
+    end
+    players
+  end
 
-	def start(players)
-		puts "#{players[0].name} goes first, sign - x"
-		players[0].sign = "x "
-		puts "#{players[1].name} goes second, sign - o"
-		players[1].sign = "o "
-		puts "Start game"
-	end
+  def start(players)
+    puts "#{players[0].name} goes first, sign - x"
+    players[0].sign = "x "
+    puts "#{players[1].name} goes second, sign - o"
+    players[1].sign = "o "
+    puts "Start game"
+  end
 
-	def play(players)
-		while !win?
-			players.each do |player|
-				self.draw
-				puts "#{player.name} turn with #{player.sign}"
-				self.write(player)
-				if win?
-					puts "#{player.name} wins!"
-					exit
-				end
-			end
-		end
-	end
+  def play(players)
+    while !win?
+      players.each do |player|
+        self.draw
+        puts "#{player.name} turn with #{player.sign}"
+        self.write(player)
+        if win?
+          puts "#{player.name} wins!"
+          exit
+        end
+      end
+    end
+  end
 
-	def write(player)
-		place = gets.chomp.split("")
-		@signs[place[0].to_i-1][place[1].to_i-1] = player.sign
-	end
+  def write(player)
+    place = gets.chomp.split("")
+    @signs[place[0].to_i-1][place[1].to_i-1] = player.sign
+  end
 
-	def draw
-		@signs.each do |i|
-			puts "#{i[0]}|#{i[1]}|#{i[2]}"
-			puts "--|--|--"
-		end
-	end
+  def draw
+    @signs.each do |i|
+      puts "#{i[0]}|#{i[1]}|#{i[2]}"
+      puts "--|--|--"
+    end
+  end
 
-	def win?
-		tikrinimai = [ [@signs[0][0], @signs[1][1], @signs[2][2]], [@signs[0][2], @signs[1][1], @signs[2][0]], @signs[0], @signs[1], @signs[2], @signs.collect{|i| i[0]}, @signs.collect{|i| i[1]}, @signs.collect{|i| i[2]}] 
-		tikrinimai.each do |i|
-			if i.same_values?
-				self.draw
-				return true
-			end
-		end
-		false
-	end
+  def win?
+    tikrinimai = [ [@signs[0][0], @signs[1][1], @signs[2][2]], [@signs[0][2], @signs[1][1], @signs[2][0]], @signs[0], @signs[1], @signs[2], @signs.collect{|i| i[0]}, @signs.collect{|i| i[1]}, @signs.collect{|i| i[2]}] 
+    tikrinimai.each do |i|
+      if i.same_values?
+        self.draw
+        return true
+      end
+    end
+    false
+  end
 
 end
 
 class Player
-	attr_accessor :name, :sign
-	def initialize
-		puts "Player name:"
-		@name = gets.chomp
-	end
-	
+  attr_accessor :name, :sign
+  def initialize
+    puts "Player name:"
+    @name = gets.chomp
+  end
+  
 end
 
 game = Game.new
