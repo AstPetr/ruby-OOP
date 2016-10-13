1c8f1847547232c6d1073286814f0bfeb77880be
Asta P <astpetr@gmail.com>
2016-10-13 14:05:06 +0300

    computer chooses randomly

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,12 @@
 class Mastermind
-	attr_reader :colors
 	def initialize
-		puts "Objective: Guess 4 colors in 9 turns"
-		@turns = 1
+		@turns = 0
 		@colors = ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"]
-		@master = nil
+		@game_master = nil
 		@player_guess = []
 		@answer = []
+		puts "Objective: Guess 4 colors in 9 turns"
+		puts @colors.join(" * ")
 	end
 	# Checks if all turns there used
 	def all_turns_used?
@@ -22,29 +22,32 @@ class Mastermind
 		answ = gets.chomp
 		if answ == "y"
 			player = Human.new
-			@master = Computer.new(@colors.dup)
+			@game_master = Computer.new(@colors)
 		else
-			player = Computer.new
-			@master = Human.new
+			player = Computer.new(@colors)
+			@game_master = Human.new(true)
+			puts "***************Computer guesses"
 		end
 		player
 	end
 	# Asks for answer
 	def ask_for_answer
-		@answer = @master.answer
+		@answer = @game_master.answer
+		puts "--------------------------"
+		puts @answer
+		puts "--------------------------"
+		return @answer
 	end
     # Plays game until player wins or no more turns left
 	def play(player)
 		ask_for_answer
-		puts @colors.join(" * ")
-		@player_guess = player.guess
 		until correct?
 			if all_turns_used?
 				puts "No more turns"
 				exit
 			end
-			puts @master.hint(@player_guess)
 			@player_guess = player.guess
+			puts @game_master.hint(@player_guess)
 			@turns +=1
 		end
 		puts "Correct!"
@@ -55,48 +58,9 @@ class Mastermind
 	end
 end
 
-class Human
+class Master	
 	attr_reader :answer
-	def initialize
-		@colors = []
-	end
-    # Chooses colors for answer
-	def random
-
-	end
-    # gives gint depending on players guess
-	def guess
-
-	end
-    # Human player guess
-	def guess
-		4.times do |i|
-			@colors[i] = gets.chomp
-		end
-		@colors
-	end
-end
-
-class Computer
-	attr_reader :answer
-	def initialize(colors)
-		puts "--------------------------"
-		puts @answer = random(colors)
-		puts "--------------------------"
-	end
-    # Chooses colors for answer
-	def random(colors)
-		k = 0
-		rand_colors = []
-		4.times do |i|
-			r = rand(8-k)
-			rand_colors[i] = colors[r]
-			colors.delete_at(r)
-			k +=1
-		end
-		rand_colors
-	end
-    # gives gint depending on players guess
+	# gives hint depending on players guess
 	def hint(guess)
 		correct_place = 0
 		equal = @answer & guess
@@ -111,6 +75,68 @@ class Computer
 		 	return "Incorrect! Guess again."
 		end 
 	end
+end
+
+class Human < Master
+	
+	def initialize(t=false)
+		# @answer = choose_colors
+		# @guess = choose_colors
+		@colors = []
+		if t == true
+			@answer = generate_answer
+		end
+	end
+    # Chooses colors for answer or guess
+	def choose_colors
+        4.times do |i|
+			@colors[i] = gets.chomp
+		end
+		@colors
+	end
+    # Human player guess
+	def guess
+		puts "Guess:"
+		choose_colors
+	end
+
+	def generate_answer
+		puts "Choose colors:"
+		choose_colors
+		# ["green",  "black", "blue", "orange"]
+	end
+end
+
+class Computer < Master
+	
+	def initialize(colors)
+		@colors = colors
+		@answer = generate_answer
+	end
+    # Chooses colors for answer
+	def random
+		colors = @colors.dup
+		k = 0
+		rand_colors = []
+		4.times do |i|
+			r = rand(8-k)
+			rand_colors[i] = colors[r]
+			colors.delete_at(r)
+			k +=1
+		end
+		rand_colors
+	end
+
+	def guess
+		guess = random
+		puts guess
+		guess
+	end
+
+	def generate_answer
+		random
+	end
+    
 end
 
 game = Mastermind.new
